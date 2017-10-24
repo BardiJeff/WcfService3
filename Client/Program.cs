4fa45fc737170768eba02c59e5cf3ebc3581f8f3
cdi9bard <cdi9bard@M3225>
2017-10-24 15:19:26 +0200

    Version Console + IHM -  Publiée

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,28 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Client
 {
     public class Program
     {
-        // Coté CLIENT // 
         static void Main(string[] args)
-        {
-            using (SRGeo.GeoClient geo = new SRGeo.GeoClient())
-            {
-                SRGeo.Pays p = geo.GetInfosPays("France");
-                Console.Out.WriteLine(" Le Nom du pays est =  " + p.Nom + "\n Sa Capitale est =  " + p.Capitale + "\n Le Nombre de ses habitants est de =  " + p.NbHabitants + "  Millions ");
+        {  
 
-                List<string> listeNomPays = geo.GetNomsPays();
-                foreach (string s in listeNomPays)
-                {
-                    Console.Out.WriteLine(s);
-                }
-                Console.In.Read();
-            }
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);
+            client.DownloadStringAsync(new Uri("http://user23.2isa.org/Service1.svc/Pays", UriKind.Absolute));
+            Console.In.Read();
         }
-    }
+
+        private static void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+
+            string str = "";
+            try
+            {
+                Console.Out.WriteLine(e.Result);
+                str = e.Result;
+            }
+            catch (Exception)
+            {               
+
+                Console.WriteLine(" Erreur 404 - Destination non trouvée, sorry !!");
+                Console.WriteLine("\n Appuyez sur ''entrée'' pour quitter la console.");
+                Console.ReadKey();
+            }            
+
+            XElement xml = XElement.Parse(str);
+            XNamespace ns = "http://schemas.microsoft.com/2003/10/Serialization/Arrays";
+
+            var noms = from p in xml.Elements(ns + "string") select p.Value;
+            foreach (string nom in noms)
+            {
+                Console.Out.WriteLine(nom);
+            }
+
+        }
+    }    
 }
